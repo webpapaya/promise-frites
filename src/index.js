0e7f570bdc86c55f78e654ef63ce8fd3a8254ec1
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-19 21:56:53 +0200

    Extracted promise-helpers from https://github.com/webPapaya/cashew to this repository.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+export const ignoreReturnFor = (fn) => (arg) => {
+  fn(arg);
+  return arg;
+};
+
+export const debug = ignoreReturnFor((arg) => console.log(arg));
+
+export const rethrowError = (fn) => (error) => Promise.resolve()
+  .then(() => fn(error))
+  .then(() => { throw error });
+
+export const delay = (seconds) =>
+  new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+
+export const waitAtLeastSeconds = (seconds) => (action) => (args) =>
+  Promise.all([action(args), delay(seconds)]).then(([actionResult]) => actionResult);
+
+export const parallel = (...args) => () =>
+  Promise.all(args.map((fn) => fn()));
+
+export const timeoutAfter = (seconds) => (action) => (args) => {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => reject('timeout'), seconds * 1000);
+    Promise.resolve(action(args)).then((result) => {
+      clearTimeout(timeoutId);
+      resolve(result);
+    });
+  });
+};
---
+++ src/index.spec.js
@@ -0,0 +1,87 @@
+import { assertThat, equalTo } from 'hamjest';
+import {
+  delay,
+  ignoreReturnFor,
+  rethrowError,
+  waitAtLeastSeconds,
+  parallel,
+  timeoutAfter
+} from './index';
+
+describe('ignoreReturnFor', () => {
+  it('ignores return value', () => Promise.resolve()
+    .then(() => '1 value')
+    .then(ignoreReturnFor(() => '2 value'))
+    .then((value) =>
+      assertThat(value, equalTo('1 value'))));
+});
+
+describe('rethrowError', () => {
+  it('ignores return value', () => Promise.resolve()
+    .then(() => { throw 'my error'; })
+    .catch(rethrowError((error) => error))
+    .then(() => assertThat(false, equalTo(true)))
+    .catch((error) =>
+      assertThat(error, equalTo('my error'))));
+});
+
+describe('waitAtLeast', () => {
+  it('waits at least 15ms', () => {
+    const startTime = +new Date();
+    return Promise.resolve()
+      .then(waitAtLeastSeconds(0.015)(() => {}))
+      .then(() => {
+        const timeDifference = new Date() - startTime;
+        assertThat(timeDifference >= 10, equalTo(true));
+      });
+  });
+});
+
+describe('parallel', () => {
+  it('executes code blocks in parallel', () => {
+    return Promise.resolve()
+      .then(parallel(
+        () => 'promise 1',
+        () => Promise.resolve('promise 2')
+      ))
+      .then(([promise1, promise2]) => {
+        assertThat(promise1, equalTo(promise1));
+        assertThat(promise2, equalTo(promise2));
+      });
+  });
+});
+
+describe('timeoutAfter', () => {
+  it('throws after 10ms', () => {
+    const timeoutFast = timeoutAfter(0.01);
+    const longRunningPromise = () => delay(0.1);
+
+    return Promise.resolve()
+      .then(timeoutFast(longRunningPromise))
+      .then(() => assertThat(false, equalTo(true)))
+      .catch((error) => assertThat(error, equalTo('timeout')));
+  });
+
+  describe('succeeds if timeout isn\'t reached', () => {
+    it('AND normal function is passed in', () => {
+      const timeoutFast = timeoutAfter(0.01);
+      return Promise.resolve()
+        .then(timeoutFast(() => 'success'))
+        .then((result) => assertThat(result, equalTo('success')))
+        .catch((error) => assertThat(false, equalTo(true)));
+    });
+
+    it('AND promise is passed in', () => {
+      const timeoutFast = timeoutAfter(0.01);
+      return Promise.resolve()
+        .then(timeoutFast(() => Promise.resolve('success')))
+        .then((result) => assertThat(result, equalTo('success')))
+        .catch((error) => assertThat(false, equalTo(true)));
+    });
+  });
+});
+
+
+
+
+
