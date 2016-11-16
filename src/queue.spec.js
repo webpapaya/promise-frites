02aaffa6094404d9ae7c45fe5741cec2fb53f5cb
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-11-16 09:13:53 +0100

    Extract queue into own file and respond a new fn, instead of executing it immediately.

--- src/queue.spec.js
+++ src/queue.spec.js
@@ -1,38 +1,41 @@
 import { assertThat, equalTo } from 'hamjest';
-
-const _queue = (args = [], ...fns) => {
-  if (fns.length === 0) { return args; }
-  const [currentFn, ...rest] = fns;
-  return Promise.resolve()
-    .then(() => currentFn(args))
-    .then((newArg) => _queue([...args, newArg], ...rest));
-};
-
-export const queue = (...fns) => _queue(void 0, ...fns);
-
+import { queue } from './index';
 
 describe('queue', () => {
   it('calls given fns sequentially', () => {
     let secondWasCalled = false;
-    return queue(
-      () => assertThat(secondWasCalled, equalTo(false)),
-      () => { secondWasCalled = true; },
-    );
+    return Promise.resolve()
+      .then(() => queue(
+        () => assertThat(secondWasCalled, equalTo(false)),
+        () => { secondWasCalled = true; },
+      ));
   });
 
-  it('AND passes arguments to the next fn', () => {
-    return queue(
-      () => 'first argument',
-      () => 'second argument',
-      (myArgument) => assertThat(myArgument, equalTo(['first argument', 'second argument'])),
-    );
+  it('AND passes return value to the next fn', () => {
+    return Promise.resolve()
+      .then(queue(
+        () => 'first argument',
+        () => 'second argument',
+        (myArgument) => assertThat(myArgument, equalTo(['first argument', 'second argument'])),
+      ));
   });
 
-  it('AND responds all arguments on resolve', () => {
-    return queue(
+  it('AND responds all return values on resolve', () => Promise.resolve()
+    .then(queue(
       () => 'first',
       () => 'second',
       () => 'third',
-    ).then((myArgument) => assertThat(myArgument, equalTo(['first', 'second', 'third'])));
+    ))
+    .then((myArgument) => assertThat(myArgument, equalTo(['first', 'second', 'third']))));
+
+  it('AND doesn\'t execute remaining items if one throws', () => {
+    let thirdWasCalled = false;
+    return Promise.resolve()
+      .then(queue(
+        () => 'first',
+        () => { throw new Error() },
+        () => { thirdWasCalled = true },
+      ))
+      .catch(() => assertThat(thirdWasCalled, equalTo(false)));
   });
 });
