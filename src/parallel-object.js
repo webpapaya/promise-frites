df15a4df51a387d3d5e9e8016b8b328fdc43fda7
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-04-01 07:55:44 +0200

    Added documentation to parallelObject.

--- src/parallel-object.js
+++ src/parallel-object.js
@@ -1,4 +1,15 @@
-
+/**
+ * Executes given promises in parallel and returns the values in an object
+ *
+ * @param object, an object containing promises
+ * @example
+ * import { parallelObject } from 'promise-frites';
+ *
+ * const values = await parallelObject({
+ *  first: Promise.resolve(1),
+ *  second: Promise.resolve(2),
+ * }); // => {first: 1, second: 2}
+ */
 export const parallelObject = (object = {}) => {
   const keys = Object.keys(object);
   const promises = keys.map((key) => object[key])
