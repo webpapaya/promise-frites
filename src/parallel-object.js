c73952905ec02c7db30decce9780ef0618146994
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-04-01 15:01:58 +0200

    Added parallel-object implementation.

--- src/parallel-object.js
+++ src/parallel-object.js
@@ -2,6 +2,7 @@
  * Executes given promises in parallel and returns the values in an object
  *
  * @param object, an object containing promises
+ * @param options, { batchSize } defines how many promises are executed in parallel (only works if functions are used in the given objects). This option is usefull when one wants to throttle the amount of parallel connections to an API.
  * @example
  * import { parallelObject } from 'promise-frites';
  *
@@ -9,11 +10,51 @@
  *  first: Promise.resolve(1),
  *  second: Promise.resolve(2),
  * }); // => {first: 1, second: 2}
+ *
+ * @example
+ * import { parallelObject } from 'promise-frites';
+ *
+ * // Only two elements will be resolved in parallel
+ * const values = await parallelObject({
+ *  first: () => Promise.resolve(1),
+ *  second: () => Promise.resolve(2),
+ *  // ...
+ * }, { batchSize: 2 }); // => {first: 1, second: 2}
  */
-export const parallelObject = (object = {}) => {
+
+const resolvePromise = (promise) => typeof promise === 'function'
+  ? promise()
+  : promise;
+
+const toBatches = (array, options) => {
+  const batchSize = options.batchSize || array.length;
+  return array.reduce((result, promiseFn, index) => {
+    if (index % batchSize === 0) {
+      result.push([]);
+    }
+    result[result.length - 1].push(promiseFn);
+    return result;
+  }, []);
+}
+
+const parallel = (promiseFns, options = {}) => {
+  const batches = toBatches(promiseFns, options);
+  const result = [];
+  const promise = batches.reduce((chunkPromise, chunks) => {
+    return chunkPromise
+      .then(() => chunks.map(resolvePromise))
+      .then((promises) => Promise.all(promises))
+      .then((promiseResult) => result.push(...promiseResult));
+  }, Promise.resolve([]));
+
+  return promise.then(() => result);
+};
+
+export const parallelObject = (object = {}, options) => {
   const keys = Object.keys(object);
-  const promises = keys.map((key) => object[key])
-  return Promise.all(promises).then((values) => {
+  const promises = keys.map((key) => object[key]);
+
+  return parallel(promises, options).then((values) => {
     return values.reduce((result, value, index) => {
       result[keys[index]] = value;
       return result;
--- src/parallel-object.spec.js
+++ src/parallel-object.spec.js
@@ -2,16 +2,31 @@ import { assertThat, hasProperties, equalTo } from 'hamjest';
 import { parallelObject } from './parallel-object';
 
 describe('parallelObject', () => {
-  it('executes code blocks in parallel, and returns thema as object', () => {
-    return parallelObject({
-        first: Promise.resolve(1),
-        second: Promise.resolve(2),
-      }).then((result) => {
-        assertThat(result, hasProperties({
-          first: 1,
-          second: 2
-        }))
-      });
+  it('with promise given, returns resolved values as object', async () => {
+    const result = await parallelObject({
+      first: Promise.resolve(1),
+      second: Promise.resolve(2),
+    });
+
+    assertThat(result, hasProperties({ first: 1, second: 2 }));
+  });
+
+  it('with functions given, returns resolved values as object', async () => {
+    const result = await parallelObject({
+      first: () => Promise.resolve(1),
+      second: () => Promise.resolve(2),
+    });
+
+    assertThat(result, hasProperties({ first: 1, second: 2 }));
+  });
+
+  it('with batchSize parameter given, returns resolved values as object', async () => {
+    const result = await parallelObject({
+      first: () => Promise.resolve(1),
+      second: () => Promise.resolve(2),
+    }, { batchSize: 1 });
+
+    assertThat(result, hasProperties({ first: 1, second: 2 }));
   });
 
   it('without any argument given returns empty object', () => {
