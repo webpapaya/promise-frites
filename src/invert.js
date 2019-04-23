93b88fdaf9d97370d34f88ff68a072c739e5a479
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-04-23 11:25:58 +0200

    Added invert function.

---
+++ src/invert.js
@@ -0,0 +1,16 @@
+/**
+ * Inverts a promise.
+ * Resolved will be converted to rejected.
+ * Rejected will be converted to resolved.
+ *
+ * @example
+ *
+ * const callWeiredApi = (args) => Promise.resolve()
+ *  .then(invert(() => fetch('something/weired'));
+ */
+export const invert = (fn) => (args) => Promise.resolve()
+  .then(() => fn(args))
+  .then(
+    (value) => { throw value },
+    (error) => error,
+  );
